5cf11aefe98d29320181207864990726853367e3
Chad Butler <chad.butler@gmail.com>
2024-05-23 14:47:37 -0700

    disabled tests for now

--- introduction/tests.py
+++ introduction/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+#class LoginViewTestCase(TestCase):
+#    def setUp(self):
+#        self.client = Client()
+
+#    def test_login_page_status_code(self):
+#        response = self.client.get("/login/")
+#        self.assertEqual(response.status_code, 200)
+
+# response = c.post("/register/)
